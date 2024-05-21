7ca44a58ed3f73cddf8013ab280bec18e8a1000e
Ajdin Dinalic <adinalic1@etf.unsa.ba>
2024-05-22 00:03:38 +0200

    Added images and logos

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,7 @@
 import { Link } from "react-router-dom";
+import facebook from "../../assets/facebook-circle.svg"
+import instagram from "../../assets/instagram-circle.svg"
+import twitter from "../../assets/twitter-circle.svg"
 
 const Footer = () => {
     return ( <>
@@ -13,6 +16,11 @@ const Footer = () => {
 <p className="font-thin">GET IN TOUCH</p>
 <p className="">Call us at +123 456 789</p>
 <p className="">support@auction.com</p>
+<div className="flex flex-row gap-3">
+<a href=""><img src={facebook} alt="" className="object-contain max-h-8" /></a>
+<a href=""><img src={instagram} alt="" className="object-contain max-h-8"/></a>
+<a href=""><img src={twitter} alt="" className="object-contain max-h-8"/></a>   
+</div>
 </div>
     </div>
     </> );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
+import logo from "../../assets/auction-logo.svg"
+import facebook from "../../assets/facebook-circle.svg"
+import instagram from "../../assets/instagram-circle.svg"
+import twitter from "../../assets/twitter-circle.svg"
 
 
 const Header = () => {
     return ( <>
     <div id="header" className=" flex flex-row w-full h-11 bg-slate-900 justify-around items-center">
-        <div className="flex flex-row">
-         <a href=""><img src="" alt="" /></a>
-<a href=""><img src="" alt="" /></a>
-<a href=""><img src="" alt="" /></a>   
+        <div className="flex flex-row gap-3">
+         <a href=""><img src={facebook} alt="" className="object-contain max-h-8 " /></a>
+<a href=""><img src={instagram} alt="" className="object-contain max-h-8" /></a>
+<a href=""><img src={twitter} alt="" className="object-contain max-h-8"/></a>   
         </div>
 <div className="flex flex-row gap-3">
     <Link to={"/log-in"}>Log in</Link>
@@ -15,6 +19,11 @@ const Header = () => {
 <Link to={"/register"}>Register</Link>
 </div>
     </div>
+    <div className="flex justify-center items-center gap-3 pt-3">
+        
+          <img src={logo} alt="" className="max-h-16"/><p id="logo" className="font-thin text-2xl">AUCTION</p>  
+
+        </div>
     </> );
 }
  
